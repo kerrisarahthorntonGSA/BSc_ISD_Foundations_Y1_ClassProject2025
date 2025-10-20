dc09a94632b24a78a83ea6eaaf04707961296512
Kerri Thornton <64016374+kerrisarahthorntonGSA@users.noreply.github.com>
2025-10-20 12:35:25 +0100

    ui part trois

--- Assets/Scripts/UIFunctions.cs
+++ Assets/Scripts/UIFunctions.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIFunctions : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public string sceneName;
+    
     void Update()
     {
-        
+        PressKeyToSetActive();
+
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);//we will have a public field that we can use the name of a scene to load
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //this will load the next scene in the build index. 0 is always loaded first, the 1 and so on
+    }
+
+    public void QuitGameBtn()
+    {
+        Application.Quit();
+        Debug.Log("Application has Quit. This will only quit when running Build");
+    }
+
+    public GameObject objectToToggle;
+    public void ActivateObjectBtn()
+    {
+        objectToToggle.SetActive(true);
+    }
+
+    public GameObject UIMenu;
+    public void PressKeyToSetActive()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            UIMenu.SetActive(true);
+        }
+
     }
 }
